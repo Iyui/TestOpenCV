5043954ebcbb64afd793359a5ea7b3b21da5880b
Administrator <Administrator@DESKTOP-2D4UEC0>
2020-04-30 10:33:53 +0800

    用方框框出识别出的图像

--- TestOpenCV/Form1.Designer.cs
+++ TestOpenCV/Form1.Designer.cs
@@ -28,13 +28,123 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(775, 450);
+            this.splitContainer1.SplitterDistance = 564;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(564, 368);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.pictureBox1);
+            this.splitContainer2.Size = new System.Drawing.Size(564, 450);
+            this.splitContainer2.SplitterDistance = 368;
+            this.splitContainer2.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(14, 13);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(181, 160);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 197);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "选取图片";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(120, 197);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "开始识别";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(775, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- TestOpenCV/Form1.cs
+++ TestOpenCV/Form1.cs
@@ -9,6 +9,7 @@ using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using System.IO;
 namespace TestOpenCV
 {
     public partial class Form1 : Form
@@ -16,32 +17,49 @@ namespace TestOpenCV
         public Form1()
         {
             InitializeComponent();
-            Test();
+            //Test();
         }
-
-        private void Test()
+        Image<Bgr, byte> img;
+        Image<Gray, byte> Grayimg;
+        private void Test(Image<Bgr, byte> img, Image<Gray, byte> Grayimg, bool save=false)
         {
-            
+
             //如果支持用显卡,则用显卡运算
             CvInvoke.UseOpenCL = CvInvoke.HaveOpenCLCompatibleGpuDevice;
 
             //构建级联分类器,利用已经训练好的数据,识别人脸
-            var face = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+            //var face = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+            var face = new CascadeClassifier("lbpcascade_animeface.xml");
+
 
             //加载要识别的图片
-            var img = new Image<Bgr, byte>("0.png");
-            var img2 = new Image<Gray, byte>(img.ToBitmap());
+            //var img = new Image<Bgr, byte>("0.png");
+            //var img2 = new Image<Gray, byte>(img.ToBitmap());
 
             //把图片从彩色转灰度
-            CvInvoke.CvtColor(img, img2, ColorConversion.Bgr2Gray);
+            CvInvoke.CvtColor(img, Grayimg, ColorConversion.Bgr2Gray);
 
             //亮度增强
-            CvInvoke.EqualizeHist(img2, img2);
+            CvInvoke.EqualizeHist(Grayimg, Grayimg);
 
             //在这一步就已经识别出来了,返回的是人脸所在的位置和大小
-            var facesDetected = face.DetectMultiScale(img2, 1.1, 10, new Size(50, 50));
+            var facesDetected = face.DetectMultiScale(Grayimg, 1.1, 10, new Size(50, 50));
 
             //循环把人脸部分切出来并保存
+           // CutandSave(facesDetected);
+            ShowCut(facesDetected);
+
+
+            //释放资源退出
+            img.Dispose();
+            Grayimg.Dispose();
+            face.Dispose();
+
+            return;
+        }
+
+        private void CutandSave(Rectangle[] facesDetected)
+        {
             int count = 0;
             var b = img.ToBitmap();
             foreach (var item in facesDetected)
@@ -54,14 +72,68 @@ namespace TestOpenCV
                 bmpOut.Save($"{count}.png", System.Drawing.Imaging.ImageFormat.Png);
                 bmpOut.Dispose();
             }
-
-            //释放资源退出
             b.Dispose();
-            img.Dispose();
-            img2.Dispose();
-            face.Dispose();
+        }
 
-            return;
+        private void ShowCut(Rectangle[] facesDetected)
+        {
+            Image rectg = img.ToBitmap();
+            foreach (var item in facesDetected)
+            {
+                var bmpRect = new Bitmap(item.Width, item.Height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+                var g = Graphics.FromImage(bmpRect);
+                rectg = DrawRectangle(rectg, item.X, item.Y, item.Width, item.Height);
+            }
+            pictureBox1.Image = rectg;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog oi = new OpenFileDialog
+            {
+                //oi.InitialDirectory = "c:\\";
+                Filter = "图片(*.jpg,*.jpeg,*.gif,*.bmp,*.png) | *.jpg;*.jpeg;*.gif;*.bmp;*.png| 所有文件(*.*) | *.*",
+                RestoreDirectory = true,
+                FilterIndex = 1
+            };
+            if (oi.ShowDialog() == DialogResult.OK)
+            {
+                var filename = oi.FileName;
+
+                Image image;
+                try
+                {
+                    image = Image.FromFile(filename);
+                }
+                catch
+                {
+                    MessageBox.Show("未能加载图片,可能时不正确的格式", "错误的预期", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                pictureBox2.Image = image;
+                img = new Image<Bgr, byte>(filename);
+                Grayimg = new Image<Gray, byte>(img.ToBitmap());
+            }
+        }
+
+        /// <summary>
+        /// 画矩形
+        /// </summary>
+        /// <param name="x">左上角的坐标</param>
+        /// <param name="y">右下角的坐标</param>
+        private Image DrawRectangle(Image image, int x, int y, int width, int height)
+        {
+            var g = Graphics.FromImage(image);
+            var pen = new Pen(Color.Red);
+            g.DrawRectangle(pen, x,y,width,height);
+            return image;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Test(img,Grayimg);
         }
     }
 }
+
+
